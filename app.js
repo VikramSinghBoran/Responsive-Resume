989a96f69288b2dba152bf8fb3946ce76c8de35d
Vikram Singh Boran <vikramsinghboran98@gmail.com>
2022-04-25 22:38:33 +0530

    some order changed.

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector("#hireMe").addEventListener('click',()=>{
+    alert("Hey you can get my contact from my CV. Thank you.")
+})
+
+console.log(document.querySelector("#hireMe"));
